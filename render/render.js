8ff56e5edb41e2b424a598a26725b864ac03d568
SW <sean@SWs-MacBook-Air.local>
2023-03-01 14:42:00 +0000

    Added basic application views

---
+++ render/render.js
@@ -0,0 +1,6 @@
+const textEditor = document.querySelector(".text-editor");
+const preview = document.querySelector(".markdown-preview");
+
+textEditor.addEventListener("keyup", evt => {
+    textEditor.innerText.add("hshjfh")
+})
